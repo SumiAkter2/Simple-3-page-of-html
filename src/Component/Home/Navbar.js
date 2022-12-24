8c3b7e1048e3e2d85f710ada38f03fa850469084
SumiAkter2 <akteralisha36@gmail.com>
2022-12-24 13:46:27 +0600

    added navbar and banner

--- src/Component/Home/Navbar.js
+++ src/Component/Home/Navbar.js
@@ -3,11 +3,31 @@ import icon from "../../assets/logo.png";
 import "./Navbar.css";
 const Navbar = () => {
   return (
-    <div className="nav-container">
-      <div className="nav-logo">
-        <img src={icon} alt="brand-logo" />
+    <div className="Container">
+      {/* nav Container */}
+      <div className="nav-container">
+        <div className="nav-logo">
+          <img src={icon} alt="brand-logo" />
+          <h1>React app</h1>
+        </div>
+        <div>
+          <button> Home</button>
+          <button>Back</button>
+          <button>Next</button>
+        </div>
       </div>
-      <div></div>
+      {/* Body Text */}
+      <div className="body-container">
+        <h1 className="">
+          Lorem ipsum dolor, sit amet consectetur Dicta mollitia 
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          similique pariatur possimus quaerat incidunt rem, minima impedit
+          ratione veniam at? Magni quas voluptatum saepe vel ratione qui, est id
+          quo.
+        </p>
+      </div>
     </div>
   );
 };
